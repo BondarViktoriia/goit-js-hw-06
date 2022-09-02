d677788ae85a9b7988cbca9c4966031d70f917db
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2022-09-02 14:50:51 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,24 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const catImage = document.createElement('li');
+const catImage = document.createElement('img');
+catImage.src =
+  'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+catImage.alt = 'White and Black Long Fur Cat';
+catImage.width = 240;
+
+const fishImage = document.createElement('img');
+fishImage.src =
+  'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+fishImage.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
+fishImage.width = 240;
+
+const hoursesImg = document.createElement('img');
+hoursesImg.src =
+  'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+hoursesImg.alt = 'Group of Horses Running';
+hoursesImg.width = 240;
+
+const imageContainer = document.querySelector('.gallery');
+imageContainer.append(catImage, fishImage, hoursesImg);
