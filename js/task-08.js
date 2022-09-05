d9c2757695b1a31bf2314c22b049426c32d90654
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2022-09-05 11:49:19 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
 
+const form = document.querySelector('.login-form')
+
+form.addEventListener('submit', onFocusSubmit)
+
+function onFocusSubmit(event) {
+    event.preventDefault()
+    const {
+        elements: { email, password },
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert('Пожалуйста, заполните все поля!')
+    }
+const formData = new FormData(event.currentTarget);    
+    console.log(formData);
+
+    formData.forEach((value, name) => {
+        console.log('onForsSubmit -> value', value);
+        console.log('onforsSubmit - > name', name)
+    });
+        event.currentTarget.reset();
+};
