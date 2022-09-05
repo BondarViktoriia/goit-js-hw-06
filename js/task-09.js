7ca23c50e167fc20031dbb647e84b46a0e64323f
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2022-09-05 12:38:15 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  widgetBody: document.querySelector('body'),
+  colorName: document.querySelector(' span.color'),
+  btnChangeColor: document.querySelector('.change-color'),
+}
+
+refs.btnChangeColor.addEventListener('click', onChangeBodyColor);
+
+function onChangeBodyColor( ) {
+  let color = getRandomHexColor();
+  refs.widgetBody.style.backgroundColor = color;
+  refs.colorName.textContent = color;
+}
