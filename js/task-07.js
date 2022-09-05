d8d7f2a6e9c0ee2d62f2bfd3b7a7de83c34eb891
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2022-09-05 11:29:13 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -4,7 +4,7 @@
 const inputChange = document.querySelector('input#font-size-control')
 const abracadabra = document.querySelector('span#text')
 
-inputChange.addEventListener('change', makeMagic)
+inputChange.addEventListener('input', makeMagic)
 
 function makeMagic(event) {
     abracadabra.style.fontSize = `${event.target.value }px`
